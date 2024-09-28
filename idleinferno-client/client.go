7de075eededda8b1812392c90f5399ee7df49f11
kvitebjorn <harintonkairu@gmail.com>
2024-09-28 17:29:01 -0600

    set up listener for client

--- idleinferno-client/client.go
+++ idleinferno-client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/url"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/kvitebjorn/idleinferno/internal/requests"
@@ -33,4 +34,56 @@ func (c *Client) Run() {
 		log.Fatalln("Failed to handshake with idleinferno server: ", err.Error())
 	}
 	log.Println("idleinferno server handshake successful!")
+
+	// Start listening for server messages
+	c.listen()
+
+	// TODO: set up a buffer to send our own messages to server
+	// probalby do `go c.listen()` after this
+}
+
+func (c *Client) sendMessage(s string) error {
+	var msg requests.Message
+	msg.Code = requests.Chatter
+	msg.Message = s
+	err := c.ws.WriteJSON(&msg)
+	if err != nil {
+		log.Println("Failed to send message to idleinferno server: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *Client) listen() {
+	for {
+		if c.ws == nil {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		break
+	}
+
+	for {
+		var msg requests.Message
+		err := c.ws.ReadJSON(&msg)
+		if err != nil {
+			log.Println("Error receiving idleinferno server message: ", err.Error())
+			c.disconnect()
+			return
+		}
+		switch msg.Code {
+		case requests.Chatter:
+			log.Println(msg.Message)
+		default:
+		}
+	}
+}
+
+func (c *Client) disconnect() {
+	if c.ws == nil {
+		return
+	}
+	c.ws.Close()
+	log.Println("Disconnected!")
 }
